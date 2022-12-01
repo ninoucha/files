d834d1ca127cd638d5bee64f0599d92fe3aede3a
ninoucha <89162536+ninoucha@users.noreply.github.com>
2022-12-01 20:35:03 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "file.h"
+#include "file.c"
+
+
+int main()
+{
+    File *maFile = initialiser();
+
+    enfiler(maFile, 4);
+    enfiler(maFile, 8);
+    enfiler(maFile, 15);
+    enfiler(maFile, 16);
+    enfiler(maFile, 23);
+    enfiler(maFile, 42);
+
+    printf("\nEtat de la file :\n");
+    afficherFile(maFile);
+
+    printf("\nJe defile %d\n", defiler(maFile));
+    printf("Je defile %d\n", defiler(maFile));
+
+    printf("\nEtat de la file :\n");
+    afficherFile(maFile);
+
+    return 0;
+}
+
